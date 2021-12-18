a4f5804b29eec29c82a122757208d47cd96e9bf8
JezerM <amyuki4@icloud.com>
2021-12-17 19:15:39 -0600

    Main file added

---
+++ index.js
@@ -0,0 +1,4 @@
+export const lightdm = window.lightdm;
+export const greeter_config = window.greeter_config;
+export const theme_utils = window.theme_utils;
+export const _ready_event = window._ready_event;
